04410829139517752520b6a9c1fc3e1abae0021f
mishaalkandapath <mishaalhere@gmail.com>
2022-09-17 01:00:02 -0400

    firebase initial files and urls

--- application.py
+++ application.py
@@ -1,2 +1,10 @@
 from flask import Flask
 
+import firebase_admin
+from firebase_admin import db
+
+cred_obj = firebase_admin.credentials.Certificate('toilet-1c321-firebase-adminsdk-iojrg-a2d280fac3.json')
+default_app = firebase_admin.initialize_app(cred_object, {
+	'databaseURL': "https://console.firebase.google.com/u/0/project/toilet-1c321/database/toilet-1c321-default-rtdb/data/~2F/OverallDB"
+	})
+
